40234c9f8bb5c607fdd21ef8f31e9dbe2408cd9a
MWCastle <matt.castle.w@gmail.com>
2023-02-07 15:49:12 -0500

    Initial Commit sorta

--- entry.sh
+++ entry.sh
@@ -7,6 +7,7 @@ set -e # Exits if passed a command with pipes
 # If they do not specify one, set it to vpn267v2
 
 echo "_________________________________________________________________________________________"
+printenv
 echo "${ENV["OVPN_FILE"]}"
 
 if [ ENV['OVPN_FILE'] == "" ]
