09691754a3a2ccd18c043d04a69a83f6cd5a1d38
MWCastle <matt.castle.w@gmail.com>
2023-02-08 11:10:39 -0500

    Initial Commit sorta

--- check_ovpn_input.rb
+++ check_ovpn_input.rb
@@ -5,8 +5,8 @@
 def main
   val = ENV['OVPN_FILE']
   puts val
-  system('ls -la /var')
-  puts Dir.glob('/var/snap/docker/common/var-lib-docker/volumes/se-als_tools_vpn_vol/_data/*')
+  system('ls -la /external/config/vpn')
+  # puts Dir.glob('/var/snap/docker/common/var-lib-docker/volumes/se-als_tools_vpn_vol/_data/*')
 end
 
 main
