697f96391ff98d1f4880337f13a06bcbffadaae5
MWCastle <matt.castle.w@gmail.com>
2023-02-08 11:18:23 -0500

    Initial Commit sorta

--- check_ovpn_input.rb
+++ check_ovpn_input.rb
@@ -3,10 +3,11 @@
 # !/usr/bin/env ruby
 
 def main
-  val = ENV['OVPN_FILE']
+  val = "/external/#{ENV['OVPN_FILE']}"
   puts val
   system('ls -la /external/config/vpn')
-  puts Dir.glob('/external/config/vpn/ovpns/*')
+  ovpns = Dir.glob('/external/config/vpn/ovpns/*')
+  puts ovpns.include?(val)
 end
 
 main
