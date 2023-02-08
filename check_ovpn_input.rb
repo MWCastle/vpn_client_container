7d2be24b9fe903ba44b5abd13c1aef7b37b5e96d
MWCastle <matt.castle.w@gmail.com>
2023-02-08 11:39:52 -0500

    Initial Commit sorta

--- check_ovpn_input.rb
+++ check_ovpn_input.rb
@@ -6,6 +6,7 @@ def main
   val = "/external/#{ENV['OVPN_FILE']}"
   puts val
   system('ls -la /external/config/vpn')
+  Dir.glob('/external/config/vpn/ovpns/*')
   ovpns = Dir.glob('/external/config/vpn/ovpns/*')
   puts ovpns.include?(val)
 end
