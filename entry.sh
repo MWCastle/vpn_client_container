73bf8f7d5f7eb7a94653c31cc9b4d0ec6d7cb620
MWCastle <matt.castle.w@gmail.com>
2023-02-20 09:14:17 -0500

    Setup Complete and Cleaned

--- entry.sh
+++ entry.sh
@@ -5,6 +5,10 @@ set -e # Exits if passed a command with pipes
 # Will return 'false' or the path to the .ovpn file matching user input
 ovpn_path=$(echo $(ruby /external/check_ovpn_input.rb))
 
+echo "OVPN PATH: "
+echo "$ovpn_path"
+
+
 # Starting up an openvpn client with the specified ovpn file
 openvpn --config "$ovpn_path"
 
